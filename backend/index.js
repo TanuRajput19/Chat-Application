a3561535787806be5f6d204e43e1ca1eba3333f5
Tanu Rajput <rajput.tanu200331@gmail.com>
2025-08-10 14:59:09 +0530

    change cors code

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 };
+app.options("*", cors(corsOptions));
 
 // ✅ Middlewares
 app.use(cors(corsOptions));
